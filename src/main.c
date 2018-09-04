4d8ff4af3577924b16a3ca8c928a9b3e2c80713c
artemisfowl <skb655952@gmail.com>
2018-09-04 08:12:46 +0530

    Added main code

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ * @file main.c
+ * @author sb
+ * @brief program to read EPUB files
+ */
+
+#include <stdio.h>
+
+/* TODO Section:
+ * 1. First check the name of the file provided in the CLI arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	/*
+	 * code will be added later
+	 */
+}
